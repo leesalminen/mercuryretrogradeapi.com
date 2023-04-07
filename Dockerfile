44c0650d40334f6bab556825bf9bd462487234f8
Lee Salminen <leesalminen@gmail.com>
2023-04-07 07:47:53 -0600

    first commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# start by pulling the python image
+FROM python:3.9-slim
+
+COPY . /srv/flask_app
+WORKDIR /srv/flask_app
+
+RUN apt-get clean \
+    && apt-get -y update
+
+RUN apt-get -y install nginx \
+    && apt-get -y install python3-dev \
+    && apt-get -y install build-essential
+
+
+RUN pip install -r requirements.txt --src /usr/local/src
+
+COPY nginx.conf /etc/nginx
+RUN chmod +x ./start.sh
+CMD ["./start.sh"]
---
+++ main.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from flask import Flask, request, jsonify
+from skyfield.api import Topos, load
+
+app = Flask(__name__)
+
+def is_mercury_retrograde(date):
+    ts = load.timescale()
+    eph = load('de440.bsp')
+
+    t = ts.utc(date.year, date.month, date.day)
+    mercury = eph['mercury']
+    earth = eph['earth']
+
+    mercury_astrometric = earth.at(t).observe(mercury)
+    ra, dec, distance = mercury_astrometric.radec()
+
+    t_plus_1_day = ts.utc(date.year, date.month, date.day) + timedelta(days=1)
+    mercury_astrometric_plus_1_day = earth.at(t_plus_1_day).observe(mercury)
+    ra_plus_1_day, dec_plus_1_day, distance_plus_1_day = mercury_astrometric_plus_1_day.radec()
+
+    return bool(ra_plus_1_day.hours < ra.hours)
+
+@app.route('/', methods=['GET'])
+def mercury_retrograde():
+    date_str = request.args.get('date', datetime.today().strftime('%Y-%m-%d'))
+    try:
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+    except ValueError:
+        return jsonify({"error": "Invalid date format. Please use 'YYYY-MM-DD' format."}), 400
+
+    is_retrograde = is_mercury_retrograde(date)
+    return jsonify({"is_retrograde": is_retrograde})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
